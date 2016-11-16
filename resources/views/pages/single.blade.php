81c847dfa153a94561934f2467e3c24e7bbabfd6
Giorgio Grasso <giorgiograsso.90@gmail.com>
2016-11-16 15:58:23 +0100

    Init comments

--- resources/views/pages/single.blade.php
+++ resources/views/pages/single.blade.php
@@ -8,5 +8,12 @@
 		<p style="margin-top: -20px" >{{$content['post_date']}} - {{$content['author']['user_nicename']}}</p>
 		{!!$content['content']!!}
 	</div>
+	<?php //var_dump($content->comments()->all()); ?>
+	@foreach ($content->comments()->get() as $comment)
+		<div>
+			<h4>{{$comment->comment_author}}</h4>
+			<p>{!!$comment->comment_content!!}</p>
+		</div>
+	@endforeach
 
 @stop
