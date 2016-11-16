075ddda608bb959effc3edca6e1b212ab0501567
Giorgio Grasso <giorgiograsso.90@gmail.com>
2016-11-16 15:58:56 +0100

    Fix single blade

--- resources/views/pages/single.blade.php
+++ resources/views/pages/single.blade.php
@@ -8,7 +8,6 @@
 		<p style="margin-top: -20px" >{{$content['post_date']}} - {{$content['author']['user_nicename']}}</p>
 		{!!$content['content']!!}
 	</div>
-	<?php //var_dump($content->comments()->all()); ?>
 	@foreach ($content->comments()->get() as $comment)
 		<div>
 			<h4>{{$comment->comment_author}}</h4>
