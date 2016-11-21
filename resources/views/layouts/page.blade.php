9b4b1ed3c2a9d9c147a39efd8d1897526a54776f
Giorgio Grasso <giorgiograsso.90@gmail.com>
2016-11-21 14:43:10 +0100

    Title

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
 
-        <title>Wordavel @if(isset($content['title'])) | {{$content['title']}} @endif</title>
+        <title>{{get_bloginfo('name')}} @if(isset($content['title'])) | {{$content['title']}} @endif</title>
 
         <meta content='width=device-width,initial-scale=1' name='viewport'>
         <meta charset="UTF-8">
